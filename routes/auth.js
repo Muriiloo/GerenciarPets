9250375323c03077329652d22d1440861ee8a372
Murilo <murilooliveira110305@gmail.com>
2024-06-15 17:15:24 -0300

    teste

--- routes/auth.js
+++ routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.post('/login', (req, res) => {
     const { username, password } = req.body;
-    if (username === 'murilo' && password === '1') {
+    if (username === 'murilo' && password === '123456') {
         req.session.user = username;
         res.status(200).json({ message: 'Login realizado com sucesso' });
     } else {
